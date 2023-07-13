59cd957ba943e75bd0eb24ca834292600123d126
Will Gerry <william.gerry@outlook.com>
2023-07-13 14:37:20 +0100

    Add golden record test for expected output values

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,12 +1,81 @@
-import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { expect } from "chai";
+import { Item, GildedRose } from "../app/gilded-rose";
 
-describe('Gilded Rose', function () {
+describe("Gilded Rose", function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it("should foo", function() {
+        const gildedRose = new GildedRose([ new Item("foo", 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).to.equal("foo");
     });
 
+    // it("should have a sellIn value", function() {
+    //     const gildedRose = new GildedRose([
+    //         new Item("foo", -1, 20),
+    //         // new Item("foo", 0, 20),
+    //         // new Item("foo", 1, 20),
+    //     ])
+
+    //     const items = gildedRose.updateQuality();
+    //     expect(items[0].sellIn).to.equal(!undefined);
+    // })
+
+    it("matches the golden record", function() {
+        const gildedRose = new GildedRose([
+            new Item("foo", -1, 20),
+            new Item("foo", 0, 20),
+            new Item("foo", 1, 20),
+            new Item("foo", 2, 20),
+            new Item("foo", 3, 20),
+            new Item("foo", 8, 20),
+            new Item("foo", 11, 20),
+            new Item("foo", 15, 20),
+            new Item("foo", 7, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", 0, 20),
+            new Item("Sulfuras, Hand of Ragnaros", 1, 20),
+            new Item("Aged Brie", -2, 20),
+            new Item("Aged Brie", 1, 20),
+            new Item("Aged Brie", 0, 20),
+            new Item("Aged Brie", 8, 20),
+            new Item("Aged Brie", 7, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 7, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", -1, 20),
+        ])
+
+        const goldenRecord = [
+            new Item("foo", -2, 18),
+            new Item("foo", -1, 18),
+            new Item("foo", 0, 19),
+            new Item("foo", 1, 19),
+            new Item("foo", 2, 19),
+            new Item("foo", 7, 19),
+            new Item("foo", 10, 19),
+            new Item("foo", 14, 19),
+            new Item("foo", 6, 19),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 20),
+            new Item("Sulfuras, Hand of Ragnaros", 0, 20),
+            new Item("Sulfuras, Hand of Ragnaros", 1, 20),
+            new Item("Aged Brie", -3, 22),
+            new Item("Aged Brie", 0, 21),
+            new Item("Aged Brie", -1, 22),
+            new Item("Aged Brie", 7, 21),
+            new Item("Aged Brie", 6, 21),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 6, 22),
+            new Item("Backstage passes to a TAFKAL80ETC concert", -1, 0),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 23),
+            new Item("Backstage passes to a TAFKAL80ETC concert", -2, 0),
+        ]
+        
+        const items = gildedRose.updateQuality();
+        expect(items).to.deep.equal(goldenRecord);
+    })
 });
